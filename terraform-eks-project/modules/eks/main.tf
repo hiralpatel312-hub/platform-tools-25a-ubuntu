8efff16bf83d540222e0b1d23a65fb39b3ebd774
hiralpatel312-hub <130260859+hiralpatel-hub@users.noreply.github.com>
2025-10-25 22:34:12 -0500

    fixing errors

--- terraform-eks-project/modules/eks/main.tf
+++ terraform-eks-project/modules/eks/main.tf
@@ -8,33 +8,43 @@ resource "aws_eks_cluster" "cluster" {
     endpoint_public_access = true
     endpoint_private_access = false
   }
+ access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
 
-  depends_on = [aws_iam_role_policy_attachment.cluster_policy]
+  depends_on = [
+    aws_iam_role.cluster_role,
+    aws_iam_role_policy_attachment.cluster_policy,
+  ]
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+  }
 }
-
 # Managed addons 
 resource "aws_eks_addon" "vpc_cni" {
-  cluster_name      = aws_eks_cluster.cluster.name
-  addon_name        = "vpc-cni"
+  cluster_name = aws_eks_cluster.cluster.name
+  addon_name   = "vpc-cni"
 
   depends_on = [aws_eks_cluster.cluster]
 }
-
 
 resource "aws_eks_addon" "ebs_csi" {
   cluster_name = aws_eks_cluster.cluster.name
   addon_name   = "aws-ebs-csi-driver"
+
+  #  EBS CSI needs IAM OIDC provider & policy attached
+  depends_on = [
+    aws_eks_cluster.cluster,
+    aws_iam_openid_connect_provider.eks,
+    aws_iam_role_policy_attachment.ebs_csi_policy
+  ]
 }
+
 resource "aws_eks_addon" "coredns" {
   cluster_name = aws_eks_cluster.cluster.name
   addon_name   = "coredns"
-
-  depends_on = [aws_eks_cluster.cluster]
-}
-
-resource "aws_eks_addon" "kube_proxy" {
-  cluster_name = aws_eks_cluster.cluster.name
-  addon_name   = "kube-proxy"
 
   depends_on = [aws_eks_cluster.cluster]
 }
