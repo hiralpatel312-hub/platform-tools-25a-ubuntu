c748d18529ac9058eb0deab9955d4bc38cb66edb
hiralpatel312-hub <130260859+hiralpatel-hub@users.noreply.github.com>
2025-10-26 00:23:49 -0500

    fixing errors

--- terraform-eks-project/modules/eks/eks_access.tf
+++ terraform-eks-project/modules/eks/eks_access.tf
@@ -69,14 +69,3 @@ resource "aws_eks_access_policy_association" "node_role_policy" {
 
   depends_on = [aws_eks_access_entry.node_role_entry]
 }
-
-#########################################
-# 4. EBS CSI Policy Attachment
-#########################################
-
-resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
-  role       = aws_iam_role.ebs_csi.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-
-  depends_on = [aws_iam_role.ebs_csi]
-}
