01fb3850c9f6f94d617ba35e441eb0e6e496924d
hiralpatel312-hub <130260859+hiralpatel-hub@users.noreply.github.com>
2025-10-25 23:52:06 -0500

    fixing errors

--- terraform-eks-project/modules/eks/main.tf
+++ terraform-eks-project/modules/eks/main.tf
@@ -51,16 +51,6 @@ data "aws_eks_cluster_auth" "cluster" {
   name = aws_eks_cluster.cluster.name
 }
 
-resource "aws_iam_openid_connect_provider" "eks" {
-  url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.oidc_cert.certificates[0].sha1_fingerprint]
-}
-
-data "tls_certificate" "oidc_cert" {
-  url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
-}
-
 resource "aws_eks_addon" "coredns" {
   cluster_name = aws_eks_cluster.cluster.name
   addon_name   = "coredns"
