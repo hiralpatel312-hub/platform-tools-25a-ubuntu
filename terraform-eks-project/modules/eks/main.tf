0b11c8c6af6129acf4852fddcd18408d59f18e29
hiralpatel312-hub <130260859+hiralpatel-hub@users.noreply.github.com>
2025-10-27 21:27:50 -0500

    fixing errors

--- terraform-eks-project/modules/eks/main.tf
+++ terraform-eks-project/modules/eks/main.tf
@@ -7,41 +7,57 @@ resource "aws_eks_cluster" "cluster" {
   version  = var.k8s_version
 
   vpc_config {
-    subnet_ids         = var.public_subnet_ids
-    security_group_ids = [var.worker_sg_id]
+    subnet_ids              = var.public_subnet_ids
+    security_group_ids      = [var.worker_sg_id]
     endpoint_public_access  = true
     endpoint_private_access = false
   }
+
   access_config {
+    # Enables both AWS API-based access and the legacy ConfigMap system
     authentication_mode = "API_AND_CONFIG_MAP"
   }
-  depends_on = [aws_iam_role.cluster_role]
+
+  depends_on = [
+    aws_iam_role.cluster_role
+  ]
 }
+
 #########################################
 # Managed Add-ons
 #########################################
+
+# AWS VPC CNI Addon - Required for pod networking
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name = aws_eks_cluster.cluster.name
   addon_name   = "vpc-cni"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 
   depends_on = [aws_eks_cluster.cluster]
 }
 
+# EBS CSI Driver - for persistent storage
 resource "aws_eks_addon" "ebs_csi" {
   cluster_name             = aws_eks_cluster.cluster.name
   addon_name               = "aws-ebs-csi-driver"
   service_account_role_arn = aws_iam_role.ebs_csi.arn
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 
   depends_on = [
     aws_eks_cluster.cluster,
     aws_iam_openid_connect_provider.eks,
-    aws_iam_role_policy_attachment.ebs_csi_policy  
+    aws_iam_role_policy_attachment.ebs_csi_policy
   ]
 }
 
+# CoreDNS - required for internal DNS resolution
 resource "aws_eks_addon" "coredns" {
   cluster_name = aws_eks_cluster.cluster.name
   addon_name   = "coredns"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
 
   depends_on = [aws_eks_cluster.cluster]
 }
