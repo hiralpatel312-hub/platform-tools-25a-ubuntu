d618ef0576a25945a753caa22c2543ebca39ec93
hiralpatel312-hub <130260859+hiralpatel-hub@users.noreply.github.com>
2025-10-28 00:04:40 -0500

    updated version to fix errors

--- terraform-eks-project/providers.tf
+++ terraform-eks-project/providers.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.70.0"
+      version = ">= 6.18.0, < 7.0.0"
     }
   }
+  required_version = ">= 1.13.4"
 }
 
 
