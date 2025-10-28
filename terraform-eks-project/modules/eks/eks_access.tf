e844bc22ebeac6f5e0e94fd7f020a68fc8723f2f
hiralpatel312-hub <130260859+hiralpatel-hub@users.noreply.github.com>
2025-10-27 23:17:40 -0500

    fixing errors

--- terraform-eks-project/modules/eks/eks_access.tf
+++ terraform-eks-project/modules/eks/eks_access.tf
@@ -2,8 +2,29 @@
 # Wait for EKS Cluster Propagation
 #########################################
 resource "time_sleep" "wait_for_cluster" {
-  create_duration = "300s"  
+  create_duration = "600s"  # 10 minutes
   depends_on      = [aws_eks_cluster.cluster]
+}
+
+#########################################
+# Optional: Poll EKS until Access Entry works
+#########################################
+resource "null_resource" "wait_for_access_entry_ready" {
+  depends_on = [aws_eks_cluster.cluster]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      for i in {1..40}; do
+        aws eks describe-access-entry \
+          --cluster-name ${aws_eks_cluster.cluster.name} \
+          --principal-arn ${var.sso_admin_role_arn} && exit 0
+        echo "Waiting for EKS access entry propagation..."
+        sleep 15
+      done
+      echo "Timed out waiting for access entry propagation"
+      exit 1
+    EOT
+  }
 }
 
 #########################################
@@ -14,7 +35,10 @@ resource "aws_eks_access_entry" "sso_admin" {
   principal_arn = var.sso_admin_role_arn
   type          = "STANDARD"
 
-  depends_on = [time_sleep.wait_for_cluster]
+  depends_on = [
+    time_sleep.wait_for_cluster,
+    null_resource.wait_for_access_entry_ready
+  ]
 }
 
 resource "aws_eks_access_policy_association" "sso_admin_policy" {
@@ -35,7 +59,10 @@ resource "aws_eks_access_entry" "github_runner" {
   principal_arn = var.github_runner_terraform_role_arn
   type          = "STANDARD"
 
-  depends_on = [time_sleep.wait_for_cluster]
+  depends_on = [
+    time_sleep.wait_for_cluster,
+    null_resource.wait_for_access_entry_ready
+  ]
 }
 
 resource "aws_eks_access_policy_association" "github_runner_policy" {
@@ -56,7 +83,9 @@ resource "aws_eks_access_entry" "node_role_entry" {
   principal_arn = aws_iam_role.node_role.arn
   type          = "EC2"
 
-  depends_on = [aws_eks_cluster.cluster]
+  depends_on = [
+    aws_eks_cluster.cluster
+  ]
 }
 
 resource "aws_eks_access_policy_association" "node_role_policy" {
