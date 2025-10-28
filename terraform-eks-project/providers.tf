c71daa3800b24416dbf188a97c47d69acd437007
hiralpatel312-hub <130260859+hiralpatel-hub@users.noreply.github.com>
2025-10-27 23:50:38 -0500

    fixing errors

--- terraform-eks-project/providers.tf
+++ terraform-eks-project/providers.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = ">= 6.70.0"
     }
   }
 }
+
 
 provider "aws" {
   region = "us-east-1"
