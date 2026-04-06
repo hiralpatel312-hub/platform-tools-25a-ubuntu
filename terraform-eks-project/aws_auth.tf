53cf84588692a1883725915e0f2f466e982a9a50
hiralpatel312-hub <130260859+hiralpatel-hub@users.noreply.github.com>
2026-04-06 00:44:14 -0400

    updated readMe

--- terraform-eks-project/aws_auth.tf
+++ terraform-eks-project/aws_auth.tf
@@ -1,20 +1,20 @@
-resource "kubernetes_config_map_v1_data" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
+# resource "kubernetes_config_map_v1_data" "aws_auth" {
+#   metadata {
+#     name      = "aws-auth"
+#     namespace = "kube-system"
+#   }
 
-  data = {
-    mapRoles = yamlencode([
-      {
-        rolearn  = module.eks.node_role_arn
-        username = "system:node:{{EC2PrivateDNSName}}"
-        groups   = ["system:bootstrappers", "system:nodes"]
-      }
-    ])
-  }
+#   data = {
+#     mapRoles = yamlencode([
+#       {
+#         rolearn  = module.eks.node_role_arn
+#         username = "system:node:{{EC2PrivateDNSName}}"
+#         groups   = ["system:bootstrappers", "system:nodes"]
+#       }
+#     ])
+#   }
 
-  force = true
+#   force = true
 
-  depends_on = [module.eks]
-}
+#   depends_on = [module.eks]
+# }
