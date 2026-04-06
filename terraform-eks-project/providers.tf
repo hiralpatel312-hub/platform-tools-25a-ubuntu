12add2ffb6648a3f0bd3097967f228cd8812afbf
hiralpatel312-hub <130260859+hiralpatel-hub@users.noreply.github.com>
2026-04-06 00:50:35 -0400

    updated readMe

---
+++ terraform-eks-project/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
